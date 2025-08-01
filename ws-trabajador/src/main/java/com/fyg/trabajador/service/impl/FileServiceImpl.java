f95c1a68718afe5402c5ef8d0f0cc8c4f27f3373
Meme <meme_2910@hotmail.com>
2025-08-01 16:59:03 -0600

    Se actualiza el puerto para que utilice el generico y se agrega logs en servicios

--- ws-trabajador/src/main/java/com/fyg/trabajador/service/impl/FileServiceImpl.java
+++ ws-trabajador/src/main/java/com/fyg/trabajador/service/impl/FileServiceImpl.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,7 @@ import com.fyg.trabajador.service.FileService;
 
 @Service("fileService")
 public class FileServiceImpl implements FileService{
+    private static final Logger LOG = LoggerFactory.getLogger(FileServiceImpl.class);
 
     @Autowired
     private Environment env;
@@ -22,11 +25,15 @@ public class FileServiceImpl implements FileService{
     @Override
     public ResponseEntity<?> generaArchivo() throws Exception {
         String ruta = env.getProperty("openshift.volumen.persistente");
+        LOG.info("RUTA->"+ruta);
         String nombreArchivo = System.currentTimeMillis() + ".csv";
+        LOG.info("NOMBRE_ARCHIVO->"+nombreArchivo);
         String archivoCsv = ruta.endsWith("/") ? ruta + nombreArchivo : ruta + "/" + nombreArchivo;
-
+        LOG.info("ARCHIVO_CSV->"+archivoCsv);
         String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+        LOG.info("NOW->"+now);
         String pod = env.getProperty("openshift.pod");
+        LOG.info("POD->"+pod);
         try {
             PrintWriter writter = new PrintWriter(new FileWriter(archivoCsv));
             writter.println("Fecha,POD");
